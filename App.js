f1b6f67846a4d5b15dca4f51bb61e1296361a8a3
kptgit <kptgit@gmail.com>
2022-09-14 08:37:52 -0300

    pamm

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
+import styled from 'styled-components';
 
 const proj = () =>{
   return (
@@ -13,7 +14,7 @@ const proj = () =>{
       color:"green",
     }}
     >
-    teste
+    
     </Text>
     {/* <Image source={{URL"https://www.magazineluiza.com.br/adaptador-wireless-usb-2-0-d-link-dwa-123/p/hf4cf3c585/in/apwf/"}}> </Image>  */}
     </View>
@@ -38,4 +39,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
 });
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`
+
+render(
+  <Container>
+    <Button>Normal Button</Button>
+    <Button primary>Primary Button</Button>
+  </Container>
+);
+
