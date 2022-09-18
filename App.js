a421663163e7bb8e6894c236f07b292b1315d7db
kptgit <112552559+kptgit@users.noreply.github.com>
2022-09-17 22:11:46 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
-import styled from 'styled-components';
-
-const proj = () =>{
-  return (
-    <ScrollView>
-      <View>
-        <Text
-    style={{
-      fontsize: 45,
-      alignSelf: "center",
-      color:"green",
-    }}
-    >
-    
-    </Text>
-    {/* <Image source={{URL"https://www.magazineluiza.com.br/adaptador-wireless-usb-2-0-d-link-dwa-123/p/hf4cf3c585/in/apwf/"}}> </Image>  */}
-    </View>
-    </ScrollView>
-  )};
-  
-
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>dwaqomwadlssssssssssssssssss</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: 'https://i.pinimg.com/736x/46/42/ae/4642ae51f2db25a5aa1641205a23a7bb.jpg' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        Teste Projeto PAM
+      </Text>
+
+      <TouchableOpacity onPress={() => alert('PAM')} style={styles.button}>
+        <Text style={styles.buttonText}>Teste</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -35,35 +19,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '000080',
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-
+  logo: {
+    width: 150,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#3274',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0.5em 1em;
-  padding: 0.25em 1em;
-
-  ${props => props.primary && css`
-    background: palevioletred;
-    color: white;
-  `}
-`;
-
-const Container = styled.div`
-  text-align: center;
-`
-
-render(
-  <Container>
-    <Button>Normal Button</Button>
-    <Button primary>Primary Button</Button>
-  </Container>
-);
-
